804e7bfc8198a1fbfbafa8478367c04324fdfd28
Stephen Cresswell <atlassian@stephen-cresswell.net>
2013-06-17 20:50:52 +0100

    README

--- example/bootstrap.js
+++ example/bootstrap.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010 Acuminous Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var rootpath = process.cwd() + '/';
 var _ = require('underscore');
 var async = require('async');   
@@ -41,7 +57,7 @@ function executeChangeSets(next) {
     var changeSets = (function() {
         var baseDir = __dirname + "/changes/";
         return _.reduce(fs.readdirSync(baseDir), function(results, file) {
-            return results.concat(require(baseDir + file).init(redis, changeLog))
+            return results.concat(require(baseDir + file).init(changeLog, redis))
         }, []);
     })();
 
